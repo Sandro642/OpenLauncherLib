44adfe5def069b2c7af96342dbf93e77bea9a445
Sandro642 <sandro33810@gmail.com>
2023-07-28 23:04:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,118 @@
+![OpenLauncherLib logo](http://image.noelshack.com/fichiers/2015/17/1429612277-oolbaniere.png)
+=====
+
+[![Say Thanks!](https://img.shields.io/badge/Say%20Thanks-!-1EAEDB.svg)](https://saythanks.io/to/Litarvan)
+
+# Launch everything you want
+
+## Internal Java Launching
+
+You can launch a Java program using the internal system. It loads some given jars, then it can launch any method from any class you want, it can be non-static, so it will use the constructor you want.
+
+```java
+List classpath = Explorer.dir("libs").files().match("^(.*\.((jar)$))*$").get();
+InternalLaunchProfile profile = new InternalLaunchProfile("fr.theshark34.MyMainClass", classpath);
+profile.launch();
+```
+
+This will launches the main(String[] args) method of the fr.theshark34.MyMainClass class, after loading all the jars in the libs folder.
+You can add a ClassInitializer, an object that will initialize the main class.
+You can also choose the method to invoke, and its parameters.
+
+## External Java Launching
+
+You can launch a Java program using the external system. It launches directly the java program, to run a simple runnable jar with its librairies.
+
+```java
+ClasspathConstructor constructor = new ClasspathConstructor();
+constructor.add(new File("mymainjar.jar"));
+constructor.add(Explorer.dir("libs").files());
+
+ExternalLaunchProfile profile = new ExternalLaunchProfile("fr.theshark34.MyClass", classpath.make());
+ExternalLauncher launcher = new ExternalLauncher(profile);
+
+Process p = launcher.launch(); // throws LaunchException
+```
+
+This will launch a java process with in classpath: mymainjar.jar and all the files of the libs folder.
+You can also add program/vm parameters, and a LaunchingEvent to customize the ProcessBuilder just before launching.
+
+## Any Program Launching
+
+You can run any executable or command, and then use the OpenLauncherLib tools with it.
+
+    Coming soon
+
+## Tools
+
+There are a lot of tools that can be used before or after the program launching.
+
+### The Single Saver
+
+The Saver uses Java Properties to save or load some data. It is useful if your launch has authentication by exemple to save the user name, or things like this.
+
+```java
+Saver saver = new Saver(new File("myfile.properties"));
+saver.set("username", "jack");
+String age = saver.get("age");
+```
+
+The data is automatically saved when you do set()
+
+### The Logger
+
+You can use the Logger to activate logging for any program you want.
+
+    Coming soon
+
+### The Process Log Manager
+
+You can use the Log Saver to save the logs to a file.
+
+    Coming soon
+
+### The Splash Screen
+
+The Splash Screen can be used to display a simple splash that you can personalize.
+
+```java
+SplashScreen splash = new SplashScreen("MySplashTitle", mySplashImage);
+splash.add(new JProgressBar());
+splash.displayFor(5000L);
+```
+
+### The Ram Selector
+
+The Ram Selector can be used to display a Ram Choosing Frame easily
+
+```java
+RamSelector selector = new RamSelector(new File("ram.txt"));
+selector.display();
+
+// Then when your launching
+String[] ramArguments = selector.getRamArguments();
+```
+
+## Minecraft
+
+There is a support for Minecraft launching, you can use these tools to create a Minecraft Launcher:
+
+### Minecraft Launcher
+
+You can use the Minecraft Launcher to create internal/external launch profile for Minecraft
+
+```java
+GameInfos infos = new GameInfos("MyMinecraft", new GameVersion("1.7.2", GameType.V1_7_2_LOWER), new GameTweak[] {GameTweak.FORGE});
+AuthInfos authInfos = new AuthInfos("PlayerUsername", "token", "uuid");
+
+InternalLaunchProfile profile = MinecraftLauncher.createInternalProfile(infos, GameFolder.BASIC, authInfos);
+InternalLauncher launcher = new InternalLauncher(profile);
+
+launcher.launch();
+```
+
+### Minecraft Crash Detector
+
+The Crash Detector detects the common Minecraft crashes
+
+    Coming soon
---
+++ src/main/java/fr/theshark34/openlauncherlib/minecraft/GameType.java
@@ -0,0 +1,536 @@
+/*
+ * Copyright 2015-2017 Adrien "Litarvan" Navratil
+ *
+ * This file is part of the OpenLauncherLib.
+
+ * The OpenLauncherLib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The OpenLauncherLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the OpenLauncherLib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.theshark34.openlauncherlib.minecraft;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * The Game Type
+ *
+ * <p>
+ *     This class contains the specifics informations about a version
+ *     or a group of verison of Minecraft.
+ *
+ *     It contains its main class, and its arguments.
+ * </p>
+ *
+ * @author Litarvan
+ * @version 3.0.4
+ * @since 2.0.0-SNAPSHOT
+ */
+public abstract class GameType
+{
+    /**
+     * The 1.5.2 or Lower game type | Forge
+     */
+    public static final GameType V1_5_2_LOWER = new GameType()
+    {
+        @Override
+        public String getName()
+        {
+            return "1.5.2 or lower";
+        }
+
+        @Override
+        public String getMainClass(GameInfos infos)
+        {
+            return "net.minecraft.launchwrapper.Launch";
+        }
+
+        @Override
+        public ArrayList<String> getLaunchArgs(GameInfos infos, GameFolder folder, AuthInfos authInfos)
+        {
+            ArrayList<String> arguments = new ArrayList<String>();
+
+            arguments.add(authInfos.getUsername());
+
+            arguments.add("token:" + authInfos.getAccessToken() + ":" + authInfos.getUuid());
+
+            arguments.add("--gameDir");
+            arguments.add(infos.getGameDir().getAbsolutePath());
+
+            arguments.add("--assetsDir");
+            File assetsDir = new File(infos.getGameDir(), folder.getAssetsFolder());
+            arguments.add(assetsDir.getAbsolutePath() + "/virtual/legacy/");
+
+            return arguments;
+        }
+    };
+
+    /**
+     * The 1.7.2 or Lower game type | Forge
+     */
+    public static final GameType V1_7_2_LOWER = new GameType()
+    {
+        @Override
+        public String getName()
+        {
+            return "1.7.2 or lower";
+        }
+
+        @Override
+        public String getMainClass(GameInfos infos)
+        {
+            return "net.minecraft.client.main.Main";
+        }
+
+        @Override
+        public ArrayList<String> getLaunchArgs(GameInfos infos, GameFolder folder, AuthInfos authInfos)
+        {
+            ArrayList<String> arguments = new ArrayList<String>();
+
+            arguments.add("--username=" + authInfos.getUsername());
+
+            arguments.add("--accessToken");
+            arguments.add(authInfos.getAccessToken());
+
+            arguments.add("--version");
+            arguments.add(infos.getGameVersion().getName());
+
+            arguments.add("--gameDir");
+            arguments.add(infos.getGameDir().getAbsolutePath());
+
+            arguments.add("--assetsDir");
+            File assetsDir = new File(infos.getGameDir(), folder.getAssetsFolder());
+            arguments.add(assetsDir.getAbsolutePath() + "/virtual/legacy/");
+
+            arguments.add("--userProperties");
+            arguments.add("{}");
+
+            arguments.add("--uuid");
+            arguments.add(authInfos.getUuid());
+
+            arguments.add("--userType");
+            arguments.add("legacy");
+
+            return arguments;
+        }
+    };
+
+    /**
+     * The 1.7.10 Game Type | Forge
+     */
+    public static final GameType V1_7_10 = new GameType()
+    {
+        @Override
+        public String getName()
+        {
+            return "1.7.10";
+        }
+
+        @Override
+        public String getMainClass(GameInfos infos)
+        {
+            return "net.minecraft.client.main.Main";
+        }
+
+        @Override
+        public ArrayList<String> getLaunchArgs(GameInfos infos, GameFolder folder, AuthInfos authInfos)
+        {
+            ArrayList<String> arguments = new ArrayList<String>();
+
+            arguments.add("--username=" + authInfos.getUsername());
+
+            arguments.add("--accessToken");
+            arguments.add(authInfos.getAccessToken());
+
+            if (authInfos.getClientToken() != null)
+            {
+                arguments.add("--clientToken");
+                arguments.add(authInfos.getClientToken());
+            }
+
+            arguments.add("--version");
+            arguments.add(infos.getGameVersion().getName());
+
+            arguments.add("--gameDir");
+            arguments.add(infos.getGameDir().getAbsolutePath());
+
+            arguments.add("--assetsDir");
+            File assetsDir = new File(infos.getGameDir(), folder.getAssetsFolder());
+            arguments.add(assetsDir.getAbsolutePath());
+
+            arguments.add("--assetIndex");
+            arguments.add(infos.getGameVersion().getName());
+
+            arguments.add("--userProperties");
+            arguments.add("{}");
+
+            arguments.add("--uuid");
+            arguments.add(authInfos.getUuid());
+
+            arguments.add("--userType");
+            arguments.add("legacy");
+
+            return arguments;
+        }
+    };
+
+    /**
+     * The 1.8 or higher Game Type | Forge
+     */
+    public static final GameType V1_8_HIGHER = new GameType()
+    {
+        @Override
+        public String getName()
+        {
+            return "1.8 or higher";
+        }
+
+        @Override
+        public String getMainClass(GameInfos infos)
+        {
+            return "net.minecraft.client.main.Main";
+        }
+
+        @Override
+        public ArrayList<String> getLaunchArgs(GameInfos infos, GameFolder folder, AuthInfos authInfos)
+        {
+            ArrayList<String> arguments = new ArrayList<String>();
+
+            arguments.add("--username=" + authInfos.getUsername());
+
+            arguments.add("--accessToken");
+            arguments.add(authInfos.getAccessToken());
+
+            if (authInfos.getClientToken() != null)
+            {
+                arguments.add("--clientToken");
+                arguments.add(authInfos.getClientToken());
+            }
+
+            arguments.add("--version");
+            arguments.add(infos.getGameVersion().getName());
+
+            arguments.add("--gameDir");
+            arguments.add(infos.getGameDir().getAbsolutePath());
+
+            arguments.add("--assetsDir");
+            File assetsDir = new File(infos.getGameDir(), folder.getAssetsFolder());
+            arguments.add(assetsDir.getAbsolutePath());
+
+            arguments.add("--assetIndex");
+
+            String version = infos.getGameVersion().getName();
+
+            int first = version.indexOf('.');
+            int second = version.lastIndexOf('.');
+
+            if (first != second)
+            {
+                version = version.substring(0, version.lastIndexOf('.'));
+            }
+
+            if (infos.getGameVersion().getName().equals("1.13.1") || infos.getGameVersion().getName().equals("1.13.2"))
+            	version = "1.13.1";
+
+            arguments.add(version);
+
+            arguments.add("--userProperties");
+            arguments.add("{}");
+
+            arguments.add("--uuid");
+            arguments.add(authInfos.getUuid());
+
+            arguments.add("--userType");
+            arguments.add("legacy");
+
+            return arguments;
+        }
+    };
+
+	/**
+	 * This is a workaround until a new version of the lib using versions JSON is published
+	 */
+	public static final GameType V1_13_HIGHER_FORGE = new GameType()
+	{
+		@Override
+		public String getName()
+		{
+			return "1.13 or higher with Forge";
+		}
+
+		@Override
+		public String getMainClass(GameInfos infos)
+		{
+			return "cpw.mods.modlauncher.Launcher";
+		}
+
+		@Override
+		public ArrayList<String> getLaunchArgs(GameInfos infos, GameFolder folder, AuthInfos authInfos)
+		{
+			ArrayList<String> arguments = new ArrayList<String>();
+
+			arguments.add("--username=" + authInfos.getUsername());
+
+			arguments.add("--accessToken");
+			arguments.add(authInfos.getAccessToken());
+
+			if (authInfos.getClientToken() != null)
+			{
+				arguments.add("--clientToken");
+				arguments.add(authInfos.getClientToken());
+			}
+
+			arguments.add("--version");
+			arguments.add(infos.getGameVersion().getName());
+
+			arguments.add("--gameDir");
+			arguments.add(infos.getGameDir().getAbsolutePath());
+
+			arguments.add("--assetsDir");
+			File assetsDir = new File(infos.getGameDir(), folder.getAssetsFolder());
+			arguments.add(assetsDir.getAbsolutePath());
+
+			arguments.add("--assetIndex");
+
+			arguments.add("1.13.1");
+
+			arguments.add("--userProperties");
+			arguments.add("{}");
+
+			arguments.add("--uuid");
+			arguments.add(authInfos.getUuid());
+
+			arguments.add("--userType");
+			arguments.add("legacy");
+
+			arguments.add("--launchTarget");
+			arguments.add("fmlclient");
+
+			arguments.add("--fml.forgeVersion");
+			arguments.add("25.0.219");
+
+			arguments.add("--fml.mcVersion");
+			arguments.add("1.13.2");
+
+			arguments.add("--fml.forgeGroup");
+			arguments.add("net.minecraftforge");
+
+			arguments.add("--fml.mcpVersion");
+			arguments.add("20190213.203750");
+
+			return arguments;
+		}
+	};
+
+    /*
+    * @dded by @Sandro642
+     */
+
+    /**
+     * The 1.16 or Higher Game Type | Forge
+     */
+
+    public static final GameType V1_16_HIGHER_FORGE = new GameType()
+    {
+        @Override
+        public String getName()
+        {
+            return "1.16 or higher with Forge";
+        }
+
+        @Override
+        public String getMainClass(GameInfos infos)
+        {
+            return "cpw.mods.modlauncher.Launcher";
+        }
+
+        @Override
+        public ArrayList<String> getLaunchArgs(GameInfos infos, GameFolder folder, AuthInfos authInfos)
+        {
+            ArrayList<String> arguments = new ArrayList<String>();
+
+            arguments.add("--username=" + authInfos.getUsername());
+
+            arguments.add("--accessToken");
+            arguments.add(authInfos.getAccessToken());
+
+            if (authInfos.getClientToken() != null)
+            {
+                arguments.add("--clientToken");
+                arguments.add(authInfos.getClientToken());
+            }
+
+            arguments.add("--version");
+            arguments.add(infos.getGameVersion().getName());
+
+            arguments.add("--gameDir");
+            arguments.add(infos.getGameDir().getAbsolutePath());
+
+            arguments.add("--assetsDir");
+            File assetsDir = new File(infos.getGameDir(), folder.getAssetsFolder());
+            arguments.add(assetsDir.getAbsolutePath());
+
+            arguments.add("--assetIndex");
+
+            // Update the asset index version for 1.16 or higher
+            arguments.add("1.16");
+
+            arguments.add("--userProperties");
+            arguments.add("{}");
+
+            arguments.add("--uuid");
+            arguments.add(authInfos.getUuid());
+
+            arguments.add("--userType");
+            arguments.add("legacy");
+
+            arguments.add("--launchTarget");
+            arguments.add("fmlclient");
+
+            arguments.add("--fml.forgeVersion");
+            // Update the Forge version for 1.16 or higher
+            arguments.add("32.0.108");
+
+            arguments.add("--fml.mcVersion");
+            // Update the Minecraft version for 1.16 or higher
+            arguments.add("1.16.1");
+            arguments.add("1.16.2");
+            arguments.add("1.16.5");
+
+            arguments.add("--fml.forgeGroup");
+            arguments.add("net.minecraftforge");
+
+            arguments.add("--fml.mcpVersion");
+            arguments.add("20210115.111550");
+
+            return arguments;
+        }
+    };
+
+
+    /**
+     * Fabric Version supported
+     * @return Fabric Version
+     *
+     * 1.19
+     * @Not-avalaible 1.20
+     */
+
+
+
+    /**
+     * The 1.19 or Higher Game Type | Fabric
+     */
+
+    public static final GameType V1_19_HIGHER_FABRIC = new GameType()
+    {
+        @Override
+        public String getName()
+        {
+            return "1.19 or higher with Fabric";
+        }
+
+        @Override
+        public String getMainClass(GameInfos infos)
+        {
+            return "net.fabricmc.loader.launch.knot.KnotClient";
+        }
+
+        @Override
+        public ArrayList<String> getLaunchArgs(GameInfos infos, GameFolder folder, AuthInfos authInfos)
+        {
+            ArrayList<String> arguments = new ArrayList<String>();
+
+            arguments.add("--username=" + authInfos.getUsername());
+
+            arguments.add("--accessToken");
+            arguments.add(authInfos.getAccessToken());
+
+            if (authInfos.getClientToken() != null)
+            {
+                arguments.add("--clientToken");
+                arguments.add(authInfos.getClientToken());
+            }
+
+            arguments.add("--version");
+            arguments.add(infos.getGameVersion().getName());
+
+            arguments.add("--gameDir");
+            arguments.add(infos.getGameDir().getAbsolutePath());
+
+            arguments.add("--assetsDir");
+            File assetsDir = new File(infos.getGameDir(), folder.getAssetsFolder());
+            arguments.add(assetsDir.getAbsolutePath());
+
+            arguments.add("--assetIndex");
+            // Update the asset index version for 1.19 or higher
+            arguments.add("1.19");
+
+            // Update the Minecraft version for 1.16 or higher
+            arguments.add("1.19.1");
+            arguments.add("1.19.2");
+
+            arguments.add("--userProperties");
+            arguments.add("{}");
+
+            arguments.add("--uuid");
+            arguments.add(authInfos.getUuid());
+
+            arguments.add("--userType");
+            arguments.add("legacy");
+
+            arguments.add("--tweakClass");
+            arguments.add("net.fabricmc.loader.launch.knot.KnotClientTweaker");
+
+            return arguments;
+        }
+    };
+
+    /**
+     * The 1.20 or Higher Game Type | Fabric
+     *
+     * For the future, not available.
+     */
+
+
+
+
+
+
+
+    /**
+     * The name of the Game Type
+     *
+     * @return Returns the name of the game type
+     */
+    public abstract String getName();
+
+    /**
+     * Returns the main class of the Minecraft Game Type
+     *
+     * @param infos The infos of the game
+     *
+     * @return The main class
+     */
+    public abstract String getMainClass(GameInfos infos);
+
+    /**
+     * Returns the launch arguments of the Minecraft Game Type
+     *
+     * @param infos     The infos of the game
+     * @param folder    The current GameFolder
+     * @param authInfos The current AuthInfos
+     *
+     * @return The launch arguments
+     */
+    public abstract ArrayList<String> getLaunchArgs(GameInfos infos, GameFolder folder, AuthInfos authInfos);
+}
