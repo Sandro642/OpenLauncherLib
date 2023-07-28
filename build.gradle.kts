6e2e5ca24264b7afbe06f16c6ecb433ccbc2cbba
Sandro642 <sandro33810@gmail.com>
2023-07-28 23:22:26 +0200

    Update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,14 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.json:json:20190722")
+
 }
 
 tasks.test {
